f22e8a789f3e417e2f4586d730d47aa7a06c0576
chhzh123 <hc676@cornell.edu>
2023-06-25 19:08:47 +0000

    Add rmsnorm

--- torch/ft_ops.cc
+++ torch/ft_ops.cc
@@ -2,12 +2,44 @@
 #include "src/fastertransformer/th_op/th_utils.h"
 #include <torch/extension.h>
 
+torch::Tensor rms_norm(const torch::Tensor& input, const torch::Tensor& weight, const double eps)
+{
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
+    int          n_dim  = input.dim();
+    int          M      = 1;
+    auto         out    = torch::zeros_like(input);
+    for (int i = 0; i < n_dim - 1; ++i)
+        M *= input.size(i);
+    int N = input.size(n_dim - 1);
+    if (input.dtype() == torch::kFloat16) {
+        fastertransformer::invokeGeneralT5LayerNorm<__half>((__half*)out.data_ptr(),
+                                                            (const __half*)input.data_ptr(),
+                                                            (const __half*)weight.data_ptr(),
+                                                            nullptr,
+                                                            eps,
+                                                            M,
+                                                            N,
+                                                            stream);
+    }
+    else {
+        fastertransformer::invokeGeneralT5LayerNorm<float>((float*)out.data_ptr(),
+                                                           (const float*)input.data_ptr(),
+                                                           (const float*)weight.data_ptr(),
+                                                           nullptr,
+                                                           eps,
+                                                           M,
+                                                           N,
+                                                           stream);
+    }
+    return out;
+}
+
 torch::Tensor add_bias_residual_layernorm(torch::Tensor        hidden_states,
-                                 const torch::Tensor& residual,
-                                 const torch::Tensor& bias,
-                                 const torch::Tensor& gamma,
-                                 const torch::Tensor& beta,
-                                 const double         eps)
+                                          const torch::Tensor& residual,
+                                          const torch::Tensor& bias,
+                                          const torch::Tensor& gamma,
+                                          const torch::Tensor& beta,
+                                          const double         eps)
 {
     cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
     int          n_dim  = hidden_states.dim();
@@ -42,10 +74,12 @@ torch::Tensor add_bias_residual_layernorm(torch::Tensor        hidden_states,
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
+    m.def("rms_norm", &rms_norm, "FT rms norm wrapper");
     m.def("add_bias_residual_layernorm", &add_bias_residual_layernorm, "FT add bias layernorm wrapper");
 }
 
 TORCH_LIBRARY(ft, m)
 {
+    m.def("rms_norm", rms_norm);
     m.def("add_bias_residual_layernorm", add_bias_residual_layernorm);
 }
